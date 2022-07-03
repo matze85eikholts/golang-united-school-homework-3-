dab7cd33fd224a4cf1e853581941bc904dce9261
unknown <matze85eikholts@gmail.com>
2022-07-03 13:06:46 +0300

    Solution for the third lesson

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,14 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
+	var sum float32 = 0.0
+	N := 0.0
+	for _, i := range input {
+		sum += i
+		if i != 0.0 {
+			N++
+		}
+	}
+	result = sum / float32(N)
 	return
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,16 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
+	keys := make([]int, 0, len(input))
+	for key := range input {
+		keys = append(keys, key)
+	}
+	sort.Ints(keys)
+	result = make([]string, 0, len(input))
+	for _, i := range keys {
+		result = append(result, input[i])
+	}
 	return
 }
